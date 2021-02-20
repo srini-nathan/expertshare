95bee92295b30e7dce7985b1ad3d97122ccc320b
oldi1992 <oldiprillo7@gmail.com>
2021-02-20 22:15:45 +0530

    Add styled-component support

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,3 @@
-import "!style-loader!css-loader!sass-loader!../node_modules/bootstrap/scss/bootstrap.scss";
-
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
---
+++ src/SharedModule/theme/expertshare.ts
@@ -0,0 +1,7 @@
+import { Theme, makeTheme } from "bootstrap-styled";
+
+const defaultTheme: Theme = makeTheme({
+    _name: "default",
+    "$brand-primary": "#36989c",
+});
+export default defaultTheme;
