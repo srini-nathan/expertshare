a28d34aaae39ed928a0fde278c3b177d5542bf8e
oldi1992 <oldiprillo7@gmail.com>
2021-02-22 15:42:58 +0530

    Add global decorator for storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,9 @@
+import React from "react";
+import BootstrapProvider from "@bootstrap-styled/provider";
+import defaultTheme from "../src/SharedModule/theme/expertshare";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+export const decorators = [(Story) => <BootstrapProvider theme={defaultTheme}><Story /></BootstrapProvider>]
