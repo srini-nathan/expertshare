7a8c35d5d23b7f49acf834bd1197399fb105a8c1
oldi1992 <oldiprillo7@gmail.com>
2021-02-22 22:48:57 +0530

    Update storybook global decorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,4 +6,4 @@ export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
 
-export const decorators = [(Story) => <BootstrapProvider theme={defaultTheme}><Story /></BootstrapProvider>]
+export const decorators = [(Story) => <BootstrapProvider theme={defaultTheme}>{Story()}</BootstrapProvider>]
