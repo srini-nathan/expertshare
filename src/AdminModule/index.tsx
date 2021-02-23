8fb9d24cb31681b9cc585848900da65bcfbaf728
oldi1992 <oldiprillo7@gmail.com>
2021-02-23 22:42:44 +0530

    Add Demo pages to Admin Module

--- src/AdminModule/index.tsx
+++ src/AdminModule/index.tsx
@@ -1,9 +1,12 @@
+import React from "react";
 import translations from "./translations";
 import ModuleConfig from "../SharedModule/models/ModuleConfig";
+import AdminModuleRouter from "./AdminModuleRouter";
 
 const moduleConfig: ModuleConfig = {
     id: "admin-module",
     translations,
+    router: <AdminModuleRouter key={"admin-module-router"} path={"/admin/*"} />,
 };
 
 export default moduleConfig;
---
+++ src/AdminModule/pages/SettingPage/SettingPage.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+import { RouteComponentProps } from "@reach/router";
+
+export const SettingPage: FC<RouteComponentProps> = (): JSX.Element => {
+    return <h1>Setting Page</h1>;
+};
+
+export default SettingPage;
