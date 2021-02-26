0751a50eeab11125e3f352333913f753b83262e5
oldi1992 <oldiprillo7@gmail.com>
2021-02-26 20:23:57 +0530

    Update storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,6 @@
 import React from "react";
-import BootstrapProvider from "@bootstrap-styled/provider";
-import defaultTheme from "../src/SharedModule/theme/expertshare";
+import '!style-loader!css-loader!sass-loader!../src/AppModule/assets/scss/app.scss';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
-
-export const decorators = [(Story) => <BootstrapProvider theme={defaultTheme}>{Story()}</BootstrapProvider>]
