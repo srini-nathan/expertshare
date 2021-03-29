6a2c7d56864bd28d1f6081b47bfa12aadba02977
oldi1992 <oldiprillo7@gmail.com>
2021-03-29 23:28:36 +0530

    WIP: AppGrid

--- src/AppModule/containers/AppGrid/AppGrid.tsx
+++ src/AppModule/containers/AppGrid/AppGrid.tsx
@@ -1,11 +1,27 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useState } from "react";
 import { AgGridColumn, AgGridReact } from "ag-grid-react";
 
 import "ag-grid-enterprise";
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 import "./assets/scss/style.scss";
-import { GridApi, GridReadyEvent } from "ag-grid-community";
+import {
+    GridApi,
+    GridReadyEvent,
+    IServerSideDatasource,
+    IServerSideGetRowsParams,
+    ServerSideStoreType,
+} from "ag-grid-community";
+import { ColumnApi } from "ag-grid-community/dist/lib/columnController/columnApi";
+import { PaginationChangedEvent } from "ag-grid-community/dist/lib/events";
+import { LanguageApi } from "../../../AdminModule/apis/LanguageApi";
+import { Language } from "../../../AdminModule/models";
+import { ListResponse } from "../../models";
+import { AppGridPagination } from "../../components/AppGridPagination";
+
+const GlobalGridConfig = {
+    pageSize: 30,
+};
 
 export interface AppGridProps {
     data: any[];
@@ -13,26 +29,75 @@ export interface AppGridProps {
 
 export const AppGrid: FC<AppGridProps> = ({ data }) => {
     const [gridApi, setGridApi] = useState<GridApi>();
+    const [, setGridColumnApi] = useState<ColumnApi>();
+    const [totalItems, setTotalItems] = useState<number>(0);
+    const [active, setActive] = useState<number>(1);
+    const onGridReady = (event: GridReadyEvent) => {
+        setGridApi(event.api);
+        setGridColumnApi(event.columnApi);
 
-    const onGridReady = (params: GridReadyEvent) => {
-        const { api } = params;
-        setGridApi(api);
+        const dataSource: IServerSideDatasource = {
+            getRows(params: IServerSideGetRowsParams) {
+                // eslint-disable-next-line no-console
+                console.log("IServerSideDatasource", params);
+                const { request } = params;
+                const { endRow } = request;
+                const pageNo = endRow / GlobalGridConfig.pageSize;
+                LanguageApi.findAll<Language>(pageNo).then(
+                    (data: ListResponse<Language>) => {
+                        setTotalItems(data.totalItems);
+                        params.successCallback(data.items, data.totalItems);
+                    }
+                );
+            },
+        };
+
+        event.api.setServerSideDatasource(dataSource);
     };
 
-    useEffect(() => {
-        gridApi?.refreshCells();
-    }, [data]);
+    const onPaginationChanged = (event: PaginationChangedEvent) => {
+        const { api } = event;
+        setActive(api.paginationGetCurrentPage() + 1);
+    };
 
     return (
-        <div
-            className="ag-theme-alpine d-flex"
-            style={{ height: 400, width: 600 }}
-        >
-            <AgGridReact onGridReady={onGridReady} rowData={data}>
-                <AgGridColumn field="make" />
-                <AgGridColumn field="model" />
-                <AgGridColumn field="price" />
-            </AgGridReact>
-        </div>
+        <React.Fragment>
+            <div className="ag-theme-alpine">
+                <AgGridReact
+                    defaultColDef={{
+                        flex: 1,
+                        minWidth: 150,
+                        sortable: true,
+                        resizable: true,
+                    }}
+                    rowModelType={"serverSide"}
+                    serverSideStoreType={ServerSideStoreType.Partial}
+                    paginationPageSize={GlobalGridConfig.pageSize}
+                    suppressPaginationPanel={true}
+                    cacheBlockSize={GlobalGridConfig.pageSize}
+                    onPaginationChanged={onPaginationChanged}
+                    pagination={true}
+                    getRowNodeId={(item) => {
+                        return item.id;
+                    }}
+                    onGridReady={onGridReady}
+                    gridOptions={{
+                        domLayout: "autoHeight",
+                    }}
+                >
+                    <AgGridColumn field="id" />
+                    <AgGridColumn field="name" />
+                    <AgGridColumn field="locale" />
+                </AgGridReact>
+            </div>
+            <br />
+            <AppGridPagination
+                totalItems={totalItems}
+                active={active}
+                onClick={(pageNumber) => {
+                    gridApi?.paginationGoToPage(pageNumber - 1);
+                }}
+            />
+        </React.Fragment>
     );
 };
