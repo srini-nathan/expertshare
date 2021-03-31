a756e4ee662bf79d9fc8bd73c10c886708e7b0f3
oldi1992 <oldiprillo7@gmail.com>
2021-03-31 15:23:27 +0530

    Add: AppSwitch

---
+++ src/AppModule/components/AppSwitch/AppSwitch.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from "react";
+import { Form } from "react-bootstrap";
+import "./assets/scss/style.scss";
+
+export interface AppSwitchProps {
+    value: string | number;
+    name?: string;
+    defaultChecked?: boolean;
+    label?: string;
+}
+
+export const AppSwitch: FC<AppSwitchProps> = ({
+    name,
+    value,
+    defaultChecked = false,
+    label = "",
+}): JSX.Element => {
+    return (
+        <Form.Check
+            type="switch"
+            className={"custom-switch"}
+            label={label}
+            name={name}
+            value={value}
+            defaultChecked={defaultChecked}
+            id={name}
+        />
+    );
+};
