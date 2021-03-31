2d51070cc7c8af9625abaa738c49dde3e6ad54d1
behnam <behnam.es.90@gmail.com>
2021-03-31 14:20:27 +0430

    last changes for styles

--- src/AppModule/containers/AppGrid/AppGrid.tsx
+++ src/AppModule/containers/AppGrid/AppGrid.tsx
@@ -53,6 +53,7 @@ export const AppGrid: FC<AppGridProps> = ({
                         sortable: true,
                         resizable: true,
                     }}
+                    rowHeight={70}
                     rowModelType={"serverSide"}
                     serverSideStoreType={ServerSideStoreType.Partial}
                     paginationPageSize={appGridConfig.pageSize}
