8946f9c71078c21861629aff72ca0df4ce1815dd
oldi1992 <oldiprillo7@gmail.com>
2021-04-10 01:04:50 +0530

    Remove: AgGridPage not required

--- src/AdminModule/pages/index.tsx
+++ src/AdminModule/pages/index.tsx
@@ -3,5 +3,4 @@ export * from "./DesignPage";
 export * from "./SettingPage";
 export * from "./Translation"; // @TODO: Do we really need suffix Page ?
 export * from "./ClientPage";
-export * from "./AgGridPage";
 export * from "./Language";
