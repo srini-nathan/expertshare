62b1ac7bad28e19db902cc39e6ff95589f781b66
oldi1992 <oldiprillo7@gmail.com>
2021-04-14 11:07:25 +0530

    Fix: Conflicts

--- src/AdminModule/pages/Language/LanguageAddPage.tsx
+++ src/AdminModule/pages/Language/LanguageAddPage.tsx
@@ -1,87 +1,93 @@
-import React, { FC, Fragment } from "react";
+import React, { FC, Fragment, useState } from "react";
 import { RouteComponentProps } from "@reach/router";
-import { Row, Col } from "react-bootstrap";
-import { SimpleObject } from "../../models";
-import {
-    AppPageHeader,
-    AppDatePicker,
-    AppFormDropdown,
-    AppTagSelect,
-} from "../../../AppModule/components";
+import { Row, Col, Form } from "react-bootstrap";
+import { AppPageHeader, AppSwitch } from "../../../AppModule/components";
 import { AppBreadcrumb } from "../../../AppModule/components/AppBreadcrumb";
-
-const options = [
-    { id: "1", value: "chocolate", label: "Chocolate" },
-    { id: "2", value: "strawberry", label: "Strawberry" },
-    { id: "3", value: "vanilla", label: "Vanilla" },
-    { id: "4", value: "apple", label: "Apple" },
-    { id: "5", value: "orange", label: "Orange" },
-    { id: "6", value: "pinapple", label: "Pinapple" },
-    { id: "7", value: "cherry", label: "Cherry" },
-    { id: "8", value: "cherry", label: "Cherry 1" },
-    { id: "9", value: "cherry", label: "Cherry 2" },
-    { id: "10", value: "cherry", label: "Cherry 3" },
-    { id: "11", value: "cherry", label: "Cherry 4" },
-    { id: "12", value: "cherry", label: "Cherry 5" },
-    { id: "13", value: "cherry", label: "Cherry 6" },
-    { id: "14", value: "cherry", label: "Cherry 7" },
-    { id: "15", value: "cherry", label: "Cherry 8" },
-    { id: "16", value: "cherry", label: "Cherry 9" },
-    { id: "17", value: "cherry", label: "Cherry 10" },
-    { id: "18", value: "cherry", label: "Cherry 11" },
-    { id: "19", value: "cherry", label: "Cherry 12" },
-    { id: "20", value: "cherry", label: "Cherry 13" },
-    { id: "21", value: "cherry", label: "Cherry 14" },
-    { id: "22", value: "cherry", label: "Cherry 15" },
-    { id: "23", value: "cherry", label: "Cherry 16" },
-    { id: "24", value: "cherry", label: "Cherry 17" },
-    { id: "25", value: "cherry", label: "Cherry 18" },
-    { id: "26", value: "cherry", label: "Cherry 19" },
-];
+import { AppButton } from "../../../AppModule/components/AppButton";
 
 export const LanguageAddPage: FC<RouteComponentProps> = (): JSX.Element => {
-    const [selectedItems, setSelectedItem] = React.useState<
-        SimpleObject<string>[]
-    >([]);
+    const [validated, setValidated] = useState(false);
+    const handleSubmit = (event: any) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+    };
     return (
         <Fragment>
             <AppBreadcrumb linkText={"Language"} linkUrl={".."} />
             <AppPageHeader title={"Add Language"} />
             <Row>
                 <Col>
-                    <AppFormDropdown
-                        id={"example"}
-                        value={"chocolate"}
-                        options={options}
-                    />
-                </Col>
-            </Row>
-            <hr />
-            <hr />
-            <Row>
-                <Col>
-                    <AppDatePicker value={new Date()} onChange={() => {}} />
-                </Col>
-            </Row>
-            <hr />
-            <Row>
-                <Col md={6}>
-                    <AppTagSelect
-                        options={options}
-                        selectedItems={selectedItems}
-                        label="Category"
-                        require
-                        description="Hi this is description for this field"
-                        onChange={(item) => {
-                            const index = selectedItems.indexOf(item);
-                            if (index !== -1) {
-                                setSelectedItem([
-                                    ...selectedItems.slice(0, index),
-                                    ...selectedItems.slice(index + 1),
-                                ]);
-                            } else setSelectedItem([...selectedItems, item]);
-                        }}
-                    />
+                    <Form
+                        noValidate
+                        validated={validated}
+                        onSubmit={handleSubmit}
+                    >
+                        <Form.Row>
+                            <Form.Group
+                                as={Col}
+                                md="4"
+                                controlId="validationCustom01"
+                            >
+                                <Form.Label>Language</Form.Label>
+                                <Form.Control
+                                    required
+                                    type="text"
+                                    placeholder="First name"
+                                    defaultValue="Mark"
+                                />
+                                <Form.Control.Feedback>
+                                    Looks good!
+                                </Form.Control.Feedback>
+                            </Form.Group>
+                            <Form.Group
+                                as={Col}
+                                md="4"
+                                controlId="validationCustom02"
+                            >
+                                <Form.Label>Locale</Form.Label>
+                                <Form.Control
+                                    required
+                                    type="text"
+                                    placeholder="Last name"
+                                    defaultValue="Otto"
+                                />
+                                <Form.Control.Feedback>
+                                    Looks good!
+                                </Form.Control.Feedback>
+                            </Form.Group>
+                            <Form.Group
+                                as={Col}
+                                md="4"
+                                controlId="validationCustom02"
+                            >
+                                <Form.Label>Active</Form.Label>
+                                <AppSwitch
+                                    id={"is-active"}
+                                    name={"isActive"}
+                                    value={true}
+                                ></AppSwitch>
+                            </Form.Group>
+                        </Form.Row>
+                        {/* @TODO: Move it to FormAction component */}
+                        <div>
+                            <hr />
+                            <div className="d-flex justify-content-end">
+                                <AppButton
+                                    type="button"
+                                    variant={"outline-primary"}
+                                    className="mr-4"
+                                >
+                                    Cancel
+                                </AppButton>
+                                <AppButton type="submit">Save</AppButton>
+                            </div>
+                        </div>
+                    </Form>
                 </Col>
             </Row>
         </Fragment>
