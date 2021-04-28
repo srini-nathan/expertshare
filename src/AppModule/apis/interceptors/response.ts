e9fae46737687cd5c01cbc96f6b1339f498950b0
mash <mash>
2021-04-28 23:50:49 +0600

    update

--- src/AppModule/apis/interceptors/response.ts
+++ src/AppModule/apis/interceptors/response.ts
@@ -12,13 +12,11 @@ export const onResponseRejected = (error: AxiosError): Promise<any> => {
 
     // status code available
     if (status) {
-        if (status === 401) {
+        if (status === 401 || status === 403) {
             navigate("/auth/login", { state: {} });
-            // return navigate("/auth/login").then(() => {
             sweetError({
                 text: "You need to login!",
             });
-            // });
         }
         if (status >= 500 && status <= 599) {
             return Promise.reject(new ServerError());
