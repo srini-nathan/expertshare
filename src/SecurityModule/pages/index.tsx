797664a7a21e192dcff53eb6c84fba5c72276f9e
behnam <behnam.es.90@gmail.com>
2021-04-29 15:27:05 +0300

    forgot password

--- src/SecurityModule/pages/index.tsx
+++ src/SecurityModule/pages/index.tsx
@@ -1,3 +1,4 @@
 export * from "./IndexPage";
 export * from "./LoginPage";
 export * from "./RegisterPage";
+export * from "./ForgotPasswordPage";
--- src/SecurityModule/routers.tsx
+++ src/SecurityModule/routers.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Redirect, Router } from "@reach/router";
-import { LoginPage, RegisterPage } from "./pages";
+import { LoginPage, RegisterPage, ForgotPasswordPage } from "./pages";
 import { ModuleRouter } from "../AppModule/models";
 
 export const routers: ModuleRouter[] = [
@@ -14,6 +14,7 @@ export const routers: ModuleRouter[] = [
                     <Redirect from={"/"} to={"login"} noThrow />
                     <LoginPage path={"login"} />
                     <RegisterPage path={"register"} />
+                    <ForgotPasswordPage path={"forgot-password"} />
                 </Router>
             );
         },
