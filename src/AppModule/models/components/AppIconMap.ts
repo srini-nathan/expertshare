be54488a6fb06804b835081c349d56781427f53d
behnam <behnam.es.90@gmail.com>
2021-04-30 00:00:02 +0300

    font awesome

--- src/AppModule/models/components/AppIconMap.ts
+++ src/AppModule/models/components/AppIconMap.ts
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from "react";
-import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import { IconDefinition } from "@fortawesome/pro-regular-svg-icons";
 
 export interface AppIconMap {
     [index: string]: PropsWithChildren<IconDefinition>;
