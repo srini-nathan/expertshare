f4fca7a4b84a4191a400bc8e20a2d44f50628f71
oldi1992 <oldiprillo7@gmail.com>
2021-04-30 13:32:43 +0530

    Update: AppPageHeader with quick filter

--- src/AppModule/components/AppListPageToolbar/AppPageHeader.tsx
+++ src/AppModule/components/AppListPageToolbar/AppPageHeader.tsx
@@ -1,30 +1,77 @@
-import React, { FC } from "react";
-import { Col, Row } from "react-bootstrap";
+import React, { FC, useEffect, useRef } from "react";
+import { Col, Form, Row } from "react-bootstrap";
 import { navigate } from "@reach/router";
+import { Subject } from "rxjs";
+import { debounceTime, distinctUntilChanged, takeUntil } from "rxjs/operators";
+import { Canceler } from "axios";
 import { AppButton } from "../AppButton";
 
 export interface AppListPageToolbarProps {
     createLink?: string;
     createLabel?: string;
+    onQuickFilterChange?: (s: string) => void;
+    cancelTokenSources?: Canceler[];
 }
 
 export const AppListPageToolbar: FC<AppListPageToolbarProps> = ({
     createLink,
     createLabel = "Create New",
+    onQuickFilterChange = () => {},
+    cancelTokenSources = [],
 }): JSX.Element => {
+    const search$ = useRef(new Subject<string>());
+    const destroy$ = new Subject<string>();
+
+    useEffect(() => {
+        search$.current
+            .pipe(
+                debounceTime(300),
+                distinctUntilChanged(),
+                takeUntil(destroy$)
+            )
+            .subscribe((search: string) => {
+                while (cancelTokenSources.length > 0) {
+                    const c = cancelTokenSources.shift();
+                    if (c) {
+                        c();
+                    }
+                }
+                onQuickFilterChange(search);
+            });
+        return () => {
+            destroy$.next();
+            destroy$.complete();
+        };
+    });
+
+    function handleQuickSearch(event: React.ChangeEvent<HTMLInputElement>) {
+        search$.current.next(event.currentTarget.value);
+    }
+
     return (
-        <Row>
-            <Col md={6} />
-            <Col md={6} className={"d-flex justify-content-end"}>
-                {createLink ? (
-                    <AppButton
-                        onClick={() => {
-                            navigate(createLink);
-                        }}
-                    >
-                        + {createLabel}
-                    </AppButton>
-                ) : null}
+        <Row className={"p-2"}>
+            <Col className={"d-flex justify-content-end"}>
+                <Form>
+                    <Form.Row>
+                        <Col>
+                            <Form.Control
+                                onChange={handleQuickSearch}
+                                type={"search"}
+                            ></Form.Control>
+                        </Col>
+                        <Col>
+                            {createLink ? (
+                                <AppButton
+                                    onClick={() => {
+                                        navigate(createLink);
+                                    }}
+                                >
+                                    + {createLabel}
+                                </AppButton>
+                            ) : null}
+                        </Col>
+                    </Form.Row>
+                </Form>
             </Col>
         </Row>
     );
