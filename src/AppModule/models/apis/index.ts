1efd9bed89bc9bb1fcc0dc173cc929d95a16732b
oldi1992 <oldiprillo7@gmail.com>
2021-04-30 12:58:57 +0530

    Add: QueryParams and type on separate file

--- src/AppModule/models/apis/index.ts
+++ src/AppModule/models/apis/index.ts
@@ -5,3 +5,4 @@ export * from "./UnprocessableEntityError";
 export * from "./UnprocessableEntityErrorResponse";
 export * from "./FinalResponse";
 export * from "./EntityNotFoundErrorResponse";
+export * from "./QueryParams";
