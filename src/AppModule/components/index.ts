828afd1c5032fa0d7aea3812860f18ce6dc75155
oldi1992 <oldiprillo7@gmail.com>
2021-04-30 13:01:37 +0530

    Update: directory level export

--- src/AppModule/components/index.ts
+++ src/AppModule/components/index.ts
@@ -20,3 +20,6 @@ export * from "./AppGridNoRowsOverlay";
 export * from "./AppGridPagination";
 export * from "./AppGridAction";
 export * from "./AppFormInputPassword";
+export * from "./AppGridNoRowsOverlay";
+export * from "./AppGridPagination";
+export * from "./AppGridAction";
