58c69f80c715a07b0a5827b54708930820a0a158
oldi1992 <oldiprillo7@gmail.com>
2021-04-30 13:00:56 +0530

    Add: new helper function for AppGrid to prepare filter parameters

--- src/AppModule/containers/AppGrid/app-grid-helpers.ts
+++ src/AppModule/containers/AppGrid/app-grid-helpers.ts
@@ -1,5 +1,10 @@
 import { IServerSideGetRowsRequest } from "ag-grid-community";
-import { AppGridSortModel, SimpleObject } from "../../models";
+import {
+    AppGridFilterModel,
+    AppGridSortModel,
+    PrimitiveObject,
+    SimpleObject,
+} from "../../models";
 
 export const buildSortParams = (
     request: IServerSideGetRowsRequest
@@ -18,4 +23,24 @@ export const buildSortParams = (
     }
 
     return orderParams;
+};
+
+export const buildFilterParams = (
+    request: IServerSideGetRowsRequest
+): PrimitiveObject => {
+    const filterModel = request.filterModel as AppGridFilterModel[];
+    const filterParams: PrimitiveObject = {};
+    const setFilter = (key: string, val: string | number | boolean): void => {
+        filterParams[key] = val;
+    };
+
+    if (filterModel) {
+        Object.entries<AppGridFilterModel>(filterModel).forEach(
+            ([rowKey, { filter }]) => {
+                setFilter(rowKey, filter);
+            }
+        );
+    }
+
+    return filterParams;
 };
