c584dce9703b95f7c1e753e10d78918f6dd819da
oldi1992 <oldiprillo7@gmail.com>
2021-05-01 21:54:04 +0530

    Update: kitchen sink

--- src/AppModule/pages/kitchen-sink/KitchenSink.tsx
+++ src/AppModule/pages/kitchen-sink/KitchenSink.tsx
@@ -19,27 +19,6 @@ const options = [
     { id: "3", value: "vanilla", label: "Vanilla" },
     { id: "4", value: "apple", label: "Apple" },
     { id: "5", value: "orange", label: "Orange" },
-    { id: "6", value: "pinapple", label: "Pinapple" },
-    { id: "7", value: "cherry", label: "Cherry" },
-    { id: "8", value: "cherry", label: "Cherry 1" },
-    { id: "9", value: "cherry", label: "Cherry 2" },
-    { id: "10", value: "cherry", label: "Cherry 3" },
-    { id: "11", value: "cherry", label: "Cherry 4" },
-    { id: "12", value: "cherry", label: "Cherry 5" },
-    { id: "13", value: "cherry", label: "Cherry 6" },
-    { id: "14", value: "cherry", label: "Cherry 7" },
-    { id: "15", value: "cherry", label: "Cherry 8" },
-    { id: "16", value: "cherry", label: "Cherry 9" },
-    { id: "17", value: "cherry", label: "Cherry 10" },
-    { id: "18", value: "cherry", label: "Cherry 11" },
-    { id: "19", value: "cherry", label: "Cherry 12" },
-    { id: "20", value: "cherry", label: "Cherry 13" },
-    { id: "21", value: "cherry", label: "Cherry 14" },
-    { id: "22", value: "cherry", label: "Cherry 15" },
-    { id: "23", value: "cherry", label: "Cherry 16" },
-    { id: "24", value: "cherry", label: "Cherry 17" },
-    { id: "25", value: "cherry", label: "Cherry 18" },
-    { id: "26", value: "cherry", label: "Cherry 19" },
 ];
 
 export const KitchenSink: FC<RouteComponentProps> = (): JSX.Element => {
@@ -54,7 +33,7 @@ export const KitchenSink: FC<RouteComponentProps> = (): JSX.Element => {
                 <Col>
                     <AppFormDropdown
                         id={"example"}
-                        value={"chocolate"}
+                        defaultValue={options[0]}
                         options={options}
                     />
                 </Col>
