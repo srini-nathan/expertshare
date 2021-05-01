8ab2bc7edbf5718258b2ff4e93fbc349d95aa115
oldi1992 <oldiprillo7@gmail.com>
2021-05-01 22:06:46 +0530

    Update: app grid helper

--- src/AppModule/containers/AppGrid/app-grid-helpers.ts
+++ src/AppModule/containers/AppGrid/app-grid-helpers.ts
@@ -1,10 +1,12 @@
 import { IServerSideGetRowsRequest } from "ag-grid-community";
 import {
     AppGridFilterModel,
+    AppGridPageSizeOption,
     AppGridSortModel,
     PrimitiveObject,
     SimpleObject,
 } from "../../models";
+import { appGridConfig } from "../../config";
 
 export const buildSortParams = (
     request: IServerSideGetRowsRequest
@@ -43,4 +45,31 @@ export const buildFilterParams = (
     }
 
     return filterParams;
+};
+
+export const pageSizeOptions = (): AppGridPageSizeOption[] => [
+    {
+        label: 10,
+        value: 10,
+    },
+    {
+        label: 30,
+        value: 30,
+    },
+    {
+        label: 50,
+        value: 50,
+    },
+    {
+        label: 100,
+        value: 100,
+    },
+    {
+        label: 200,
+        value: 200,
+    },
+];
+
+export const defaultPageSize = (): AppGridPageSizeOption => {
+    return { label: appGridConfig.pageSize, value: appGridConfig.pageSize };
 };
