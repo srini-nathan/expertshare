f11e50465c138138732b00adb1e9ab5a75c4cc4f
oldi1992 <oldiprillo7@gmail.com>
2021-05-01 22:06:25 +0530

    Add: model

--- src/AppModule/models/containers/index.ts
+++ src/AppModule/models/containers/index.ts
@@ -2,3 +2,4 @@ export * from "./AppGridSortModel";
 export * from "./AppGridColDefParams";
 export * from "./AppGridActionParams";
 export * from "./AppGridFilterModel";
+export * from "./AppGridPageSizeOption";
