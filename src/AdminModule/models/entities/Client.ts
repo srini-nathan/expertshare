34a5be85054aeb7ed2624aaf7312647a1a2805c8
oldi1992 <oldiprillo7@gmail.com>
2021-05-09 14:56:14 +0530

    Update: ClientEntity with types Add: PackageEntity

--- src/AdminModule/models/entities/Client.ts
+++ src/AdminModule/models/entities/Client.ts
@@ -12,7 +12,7 @@ export class ClientEntity {
 
     notes: string;
 
-    packages: Package[];
+    packages: string[] | Package[];
 
     constructor() {
         this.name = "";
--- src/AdminModule/models/entities/Package.ts
+++ src/AdminModule/models/entities/Package.ts
@@ -3,3 +3,11 @@ import { BaseEntity } from "../../../AppModule/models";
 export interface Package extends BaseEntity {
     packageKey: string;
 }
+
+export class PackageEntity {
+    packageKey: string;
+
+    constructor(packageKey: string) {
+        this.packageKey = packageKey;
+    }
+}
