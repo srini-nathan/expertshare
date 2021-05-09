c2ba3cbfbe7f88463f4153dfef3e01b66933e513
oldi1992 <oldiprillo7@gmail.com>
2021-05-09 19:35:26 +0530

    Add: ContainerEntity

--- src/AdminModule/models/entities/Container.ts
+++ src/AdminModule/models/entities/Container.ts
@@ -1,6 +1,7 @@
 import { BaseEntity } from "../../../AppModule/models";
 import { Package } from "./Package";
 import { ContainerConfiguration } from "./ContainerConfiguration";
+import { ClientEntity } from "./Client";
 
 export interface Container extends BaseEntity {
     domain: string;
@@ -16,4 +17,39 @@ export interface Container extends BaseEntity {
     configurationTypes: ContainerConfiguration;
     designConfigurationTypes: ContainerConfiguration;
     packages: Package[];
+}
+
+export class ContainerEntity {
+    domain: string;
+
+    name: string;
+
+    containerGroup?: string;
+
+    storage: string;
+
+    bucketKey?: string;
+
+    bucketSecret?: string;
+
+    bucketName?: string;
+
+    bucketRegion?: string;
+
+    isActive?: boolean;
+
+    client?: string | ClientEntity;
+
+    description?: string;
+
+    packages: string[] | Package[];
+
+    configuration?: string[];
+
+    constructor() {
+        this.domain = "";
+        this.name = "";
+        this.storage = "Local";
+        this.packages = [];
+    }
 }
