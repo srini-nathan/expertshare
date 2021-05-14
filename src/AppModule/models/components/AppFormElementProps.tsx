cd08a1f401ddeb88a17f158520909a1bb1185fb8
oldi1992 <oldiprillo7@gmail.com>
2021-05-14 14:04:14 +0530

    Re-Factor: AppFormInput

---
+++ src/AppModule/models/components/AppFormElementProps.tsx
@@ -0,0 +1,8 @@
+export interface AppFormElementProps {
+    id?: string;
+    placeholder?: string | boolean;
+    required?: boolean;
+    label?: string;
+    description?: string;
+    className?: string;
+}
