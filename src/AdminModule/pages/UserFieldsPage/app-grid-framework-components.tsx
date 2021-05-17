24bc81995ad8105db0a9977dcefad3bf36931a91
jitinder singh <jitinder.singhtechbitsolution@outlook.com>
2021-05-17 12:51:27 +0530

    changes

--- src/AdminModule/pages/UserFieldsPage/app-grid-framework-components.tsx
+++ src/AdminModule/pages/UserFieldsPage/app-grid-framework-components.tsx
@@ -3,15 +3,15 @@ import {
     AppGridAction,
     AppGridActionProps,
 } from "../../../AppModule/components";
-import { Email } from "../../models";
-import { CellActionWithRenderParams } from "../LanguagePage/app-grid-action";
+import { UserFields } from "../../models";
+import { AppCellActionWithRenderParams } from "../../../AppModule/models";
 
 export const appGridFrameworkComponents = {
     appGridActionRenderer: (
-        params: CellActionWithRenderParams
+        params: AppCellActionWithRenderParams
     ): ReactElement => {
         const { data, onPressDelete } = params;
-        const { id } = data as Email;
+        const { id } = data as UserFields;
 
         const props: AppGridActionProps = {
             editAction: {
