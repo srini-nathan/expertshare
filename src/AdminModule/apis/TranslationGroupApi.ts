1b0e304a4592dd2d3f02762b5f57b08fb3651f94
oldi1992 <oldiprillo7@gmail.com>
2021-05-20 13:11:56 +0530

    Fix: Typos Add: constant to use defined paths

--- src/AdminModule/apis/TranslationGroupApi.ts
+++ src/AdminModule/apis/TranslationGroupApi.ts
@@ -1,11 +1,16 @@
 import { EntityAPI } from "../../AppModule/apis/EntityAPI";
 import { ROUTES } from "../../config";
 
+const {
+    api_translation_groups_post_collection: API_TRANSLATION_GROUPS_POST_COLLECTION,
+    api_translation_groups_patch_item: API_TRANSLATION_GROUPS_PATCH_COLLECTION,
+    api_translation_groups_delete_item: API_TRANSLATION_GROUPS_DELETE_COLLECTION,
+} = ROUTES;
+
 export abstract class TranslationGroupApi extends EntityAPI {
-    protected static POST_COLLECTION =
-        ROUTES.api_translation_groups_post_collection;
+    protected static POST_COLLECTION = API_TRANSLATION_GROUPS_POST_COLLECTION;
 
-    protected static PATCH_ITEM = ROUTES.api_translation_groups_patch_item;
+    protected static PATCH_ITEM = API_TRANSLATION_GROUPS_PATCH_COLLECTION;
 
-    protected static DELETE_ITEM = ROUTES.api_translation_groups_delete_item;
+    protected static DELETE_ITEM = API_TRANSLATION_GROUPS_DELETE_COLLECTION;
 }
--- src/AdminModule/apis/TranslationValueApi.ts
+++ src/AdminModule/apis/TranslationValueApi.ts
@@ -1,9 +1,13 @@
 import { EntityAPI } from "../../AppModule/apis/EntityAPI";
 import { ROUTES } from "../../config";
 
-export abstract class TranslationValueApi extends EntityAPI {
-    protected static POST_COLLECTION =
-        ROUTES.api_translation_values_post_collection;
+const {
+    api_translation_values_post_collection: API_TRANSLATION_VALUES_POST_COLLECTION,
+    api_translation_values_patch_item: API_TRANSLATION_VALUES_PATCH_COLLECTION,
+} = ROUTES;
 
-    protected static PATCH_ITEM = ROUTES.api_translation_values_patch_item;
+export abstract class TranslationValueApi extends EntityAPI {
+    protected static POST_COLLECTION = API_TRANSLATION_VALUES_POST_COLLECTION;
+
+    protected static PATCH_ITEM = API_TRANSLATION_VALUES_PATCH_COLLECTION;
 }
