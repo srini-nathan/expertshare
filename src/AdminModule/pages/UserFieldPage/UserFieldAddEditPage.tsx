34603a228291992636907161e7183b4129f88d3f
oldi1992 <oldiprillo7@gmail.com>
2021-05-28 12:41:54 +0530

    Update: UserField add/edit page

--- src/AdminModule/pages/UserFieldPage/UserFieldAddEditPage.tsx
+++ src/AdminModule/pages/UserFieldPage/UserFieldAddEditPage.tsx
@@ -1,9 +1,8 @@
 import React, { FC, Fragment, useState, useEffect } from "react";
-import { RouteComponentProps, useNavigate, useParams } from "@reach/router";
+import { RouteComponentProps } from "@reach/router";
 import { Row, Col, Form } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
 import { forEach as _forEach, find as _find } from "lodash";
 import { CONSTANTS } from "../../../config";
 import {
@@ -25,51 +24,27 @@ import { AppFormInput } from "../../../AppModule/components/AppFormInput";
 import { AppFormSelect } from "../../../AppModule/components/AppFormSelect";
 import { AppFieldTypeElement } from "../../components";
 
+import { schema, validations } from "./schema";
+import { useNavigator, useParamId } from "../../../AppModule/hooks";
+
 const { UserField } = CONSTANTS;
 const { FIELDTYPE } = UserField;
+const { name, fieldKey, labelKey } = validations;
 
-const schema = yup.object().shape({
-    name: yup.string().required(),
-    fieldKey: yup.string().required(),
-    labelKey: yup.string().required(),
-    fieldType: yup.string().required(),
-    attr: yup.array().optional().nullable(),
-    options: yup.array().when("fieldType", {
-        is:
-            FIELDTYPE.FIELDTYPE_SELECT ||
-            FIELDTYPE.FIELDTYPE_MULTI_SELECT ||
-            FIELDTYPE.FIELDTYPE_RADIO_GROUP ||
-            FIELDTYPE.FIELDTYPE_CHECKBOX_GROUP,
-        then: yup
-            .array()
-            .of(
-                yup.object().shape({
-                    key: yup.string().trim().required("option key is required"),
-                    value: yup
-                        .string()
-                        .trim()
-                        .required("option value is required"),
-                })
-            )
-            .min(1, "Minimum of 1 option"),
-    }),
-});
+const options = Object.entries(FIELDTYPE).map(([key, value]) => ({
+    value,
+    label: key,
+}));
 
 export const UserFieldAddEditPage: FC<RouteComponentProps> = ({
     navigate,
 }): JSX.Element => {
-    const { id = null } = useParams();
-    const isEditMode: boolean = id !== null;
-    const hookNav = useNavigate();
-    const nav = navigate ?? hookNav;
+    const { id, isEditMode } = useParamId();
+    const navigator = useNavigator(navigate);
 
     const [data, setData] = useState<UserFieldEntity>(new UserFieldEntity());
     const [loading, setLoading] = useState<boolean>(isEditMode);
     const [selected, setSelected] = useState<any>();
-    const options = Object.entries(FIELDTYPE).map(([key, value]) => ({
-        value,
-        label: key,
-    }));
 
     const {
         register,
@@ -78,6 +53,7 @@ export const UserFieldAddEditPage: FC<RouteComponentProps> = ({
         formState,
         setError,
         setValue,
+        trigger,
     } = useForm({
         resolver: yupResolver(schema),
         mode: "all",
@@ -87,6 +63,8 @@ export const UserFieldAddEditPage: FC<RouteComponentProps> = ({
         UserFieldApi.createOrUpdate<UserFieldEntity>(id, formData).then(
             ({ error, errorMessage }) => {
                 if (error instanceof UnprocessableEntityErrorResponse) {
+                    // @TODO: need to replace this block with setViolations,
+                    // need to fix an issue with the function
                     const { violations } = error;
                     _forEach(violations, (value: string, key: string) => {
                         const propertyName = key as keyof UserFieldEntity;
@@ -98,7 +76,7 @@ export const UserFieldAddEditPage: FC<RouteComponentProps> = ({
                 } else if (errorMessage) {
                     errorToast(errorMessage);
                 } else {
-                    nav("..").then(() => {
+                    navigator("..").then(() => {
                         successToast(
                             isEditMode
                                 ? "User Fields updated"
@@ -109,6 +87,7 @@ export const UserFieldAddEditPage: FC<RouteComponentProps> = ({
             }
         );
     };
+
     const renderOptions = (type: any) => {
         if (
             type ===
@@ -121,6 +100,7 @@ export const UserFieldAddEditPage: FC<RouteComponentProps> = ({
         }
         return false;
     };
+
     useEffect(() => {
         if (isEditMode) {
             UserFieldApi.findById<UserFieldEntity>(id).then(
@@ -132,12 +112,13 @@ export const UserFieldAddEditPage: FC<RouteComponentProps> = ({
                     } else if (response !== null) {
                         setData(response);
                         setSelected(response.fieldType);
+                        trigger();
                     }
                     setLoading(false);
                 }
             );
         }
-    }, [id, isEditMode]);
+    }, [id, isEditMode, trigger]);
 
     if (loading) {
         return <AppLoader />;
@@ -156,93 +137,83 @@ export const UserFieldAddEditPage: FC<RouteComponentProps> = ({
                     <Form noValidate onSubmit={handleSubmit(onSubmit)}>
                         <AppCard>
                             <Row>
-                                <Col md={6} sm={12}>
-                                    <AppFormInput
-                                        name={"name"}
-                                        label={"User Field Name"}
-                                        md={12}
-                                        lg={12}
-                                        sm={12}
-                                        xl={12}
-                                        required={true}
-                                        {...validation(
-                                            "name",
-                                            formState,
-                                            isEditMode
-                                        )}
-                                        errorMessage={errors.name?.message}
-                                        defaultValue={data.name}
-                                        control={control}
-                                    />
-                                    <AppFormInput
-                                        name={"fieldKey"}
-                                        label={"User Field Key"}
-                                        md={12}
-                                        lg={12}
-                                        sm={12}
-                                        xl={12}
-                                        required={true}
-                                        {...validation(
-                                            "fieldKey",
-                                            formState,
-                                            isEditMode
-                                        )}
-                                        errorMessage={errors.fieldKey?.message}
-                                        defaultValue={data.fieldKey}
-                                        control={control}
-                                    />
-                                </Col>
-                                <Col md={6} sm={12}>
-                                    <AppFormInput
-                                        name={"labelKey"}
-                                        label={"User Label Key"}
-                                        md={12}
-                                        lg={12}
-                                        sm={12}
-                                        xl={12}
-                                        required={true}
-                                        {...validation(
-                                            "labelKey",
-                                            formState,
-                                            isEditMode
-                                        )}
-                                        errorMessage={errors.labelKey?.message}
-                                        defaultValue={data.labelKey}
-                                        control={control}
-                                    />
-                                    <AppFormSelect
-                                        id={"ddFieldType"}
-                                        name={"fieldType"}
-                                        label={"Select fieldType"}
-                                        md={12}
-                                        lg={12}
-                                        xl={12}
-                                        required={true}
-                                        {...validation(
-                                            "fieldType",
-                                            formState,
-                                            isEditMode
-                                        )}
-                                        defaultValue={data.fieldType}
-                                        placeholder={"Field Type"}
-                                        errorMessage={errors.fieldType?.message}
-                                        options={options}
-                                        control={control}
-                                        transform={{
-                                            output: (
-                                                fieldType: PrimitiveObject
-                                            ) => {
-                                                setSelected(fieldType.value);
-                                                return fieldType?.value;
-                                            },
-                                            input: (value: string) => {
-                                                return _find(options, {
-                                                    value,
-                                                });
-                                            },
-                                        }}
-                                    />
-                                </Col>
+                                <AppFormInput
+                                    name={"name"}
+                                    label={"User Field Name"}
+                                    maxCount={name.max}
+                                    lg={6}
+                                    xl={6}
+                                    {...validation(
+                                        "name",
+                                        formState,
+                                        isEditMode
+                                    )}
+                                    errorMessage={errors.name?.message}
+                                    defaultValue={data.name}
+                                    control={control}
+                                />
+                                <AppFormInput
+                                    name={"fieldKey"}
+                                    label={"User Field Key"}
+                                    maxCount={fieldKey.max}
+                                    lg={6}
+                                    xl={6}
+                                    {...validation(
+                                        "fieldKey",
+                                        formState,
+                                        isEditMode
+                                    )}
+                                    errorMessage={errors.fieldKey?.message}
+                                    defaultValue={data.fieldKey}
+                                    control={control}
+                                />
+                            </Row>
+                            <Row>
+                                <AppFormInput
+                                    name={"labelKey"}
+                                    label={"User Label Key"}
+                                    maxCount={labelKey.max}
+                                    lg={6}
+                                    xl={6}
+                                    {...validation(
+                                        "labelKey",
+                                        formState,
+                                        isEditMode
+                                    )}
+                                    errorMessage={errors.labelKey?.message}
+                                    defaultValue={data.labelKey}
+                                    control={control}
+                                />
+                                <AppFormSelect
+                                    id={"ddFieldType"}
+                                    name={"fieldType"}
+                                    label={"Select fieldType"}
+                                    lg={6}
+                                    xl={6}
+                                    {...validation(
+                                        "fieldType",
+                                        formState,
+                                        isEditMode
+                                    )}
+                                    defaultValue={data.fieldType}
+                                    placeholder={"Field Type"}
+                                    errorMessage={errors.fieldType?.message}
+                                    options={options}
+                                    control={control}
+                                    transform={{
+                                        output: (
+                                            fieldType: PrimitiveObject
+                                        ) => {
+                                            setSelected(fieldType.value);
+                                            return fieldType?.value;
+                                        },
+                                        input: (value: string) => {
+                                            return _find(options, {
+                                                value,
+                                            });
+                                        },
+                                    }}
+                                />
                             </Row>
                             {selected && (
                                 <AppFieldTypeElement
@@ -282,6 +253,8 @@ export const UserFieldAddEditPage: FC<RouteComponentProps> = ({
                                         defaultChecked={data.isActive}
                                         register={register}
                                     />
+                                </Col>
+                                <Col md={4} sm={6}>
                                     <AppFormCheckBox
                                         className="container-checkbox"
                                         name={"isExport"}
@@ -302,6 +275,8 @@ export const UserFieldAddEditPage: FC<RouteComponentProps> = ({
                                         defaultChecked={data.isImport}
                                         register={register}
                                     />
+                                </Col>
+                                <Col md={4} sm={6}>
                                     <AppFormCheckBox
                                         className="container-checkbox"
                                         name={"isVcf"}
@@ -327,7 +302,7 @@ export const UserFieldAddEditPage: FC<RouteComponentProps> = ({
                                 </Col>
                                 <AppFormActions
                                     isEditMode={isEditMode}
-                                    navigation={nav}
+                                    navigation={navigator}
                                 />
                             </Row>
                         </AppCard>
