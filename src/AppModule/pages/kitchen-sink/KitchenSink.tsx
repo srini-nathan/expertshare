53ef9d6cf2a457d470f04bee14dcf4581832c5fc
oldi1992 <oldiprillo7@gmail.com>
2021-05-31 13:06:13 +0530

    Remove: AppUploader from kitchen sink

--- src/AppModule/pages/kitchen-sink/KitchenSink.tsx
+++ src/AppModule/pages/kitchen-sink/KitchenSink.tsx
@@ -17,7 +17,6 @@ import {
     AppYoutubeFrame,
     AppVimeoFrame,
     AppFormRadioSwitch,
-    AppUploader,
     AppFormRichTextArea,
 } from "../../components";
 import { SimpleObject } from "../../models";
@@ -271,12 +270,6 @@ export const KitchenSink: FC<RouteComponentProps> = (): JSX.Element => {
                     withCounter={true}
                     errorMessage="This field is required"
                     control={control}
-                />
-            </Row>
-            <Row>
-                <AppUploader
-                    accept="image/*" // For more information see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/Input
-                    withCropper
                 />
             </Row>
         </Fragment>
