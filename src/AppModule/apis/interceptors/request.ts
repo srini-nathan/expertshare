d57a6ce6e85da75afbfec1c0685b11e1727ad077
oldi1992 <oldiprillo7@gmail.com>
2021-06-01 12:40:21 +0530

    Add: ES-TIMEZONE header

--- src/AppModule/apis/interceptors/request.ts
+++ src/AppModule/apis/interceptors/request.ts
@@ -1,12 +1,14 @@
 import { AxiosRequestConfig } from "axios";
 import { AUTH_TOKEN_KEY } from "../../config/app-env";
-import { generateKeyHeader } from "../../utils/api";
+import { generateKeyHeader, getUserTimeZone } from "../../utils/api";
 
 export const onRequestFulfilled = (
     config: AxiosRequestConfig
 ): AxiosRequestConfig | Promise<AxiosRequestConfig> => {
     // eslint-disable-next-line no-console,no-param-reassign
     config.headers["ES-KEY"] = generateKeyHeader();
+    config.headers["ES-TIMEZONE"] = getUserTimeZone();
+    // @TODO: why we need this, ask daniel ?
     config.headers["Content-Type"] = "application/ld+json";
 
     const token = localStorage.getItem(AUTH_TOKEN_KEY);
