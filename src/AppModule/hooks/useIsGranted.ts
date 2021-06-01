1952a726731385d6317c2ef9c68ee01fd142b2a1
oldi1992 <oldiprillo7@gmail.com>
2021-06-01 12:57:07 +0530

    Add: util function for isGranted and custom hook useIsGranted

---
+++ src/AppModule/hooks/useIsGranted.ts
@@ -0,0 +1,7 @@
+import { useAuthState } from "./useAuthState";
+import { isGranted } from "../utils";
+
+export function useIsGranted(roleName: string) {
+    const { role } = useAuthState();
+    return isGranted(role, roleName);
+}
