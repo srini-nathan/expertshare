a13641dd34bf2b41c95f3d4e955070d823a9aae4
oldi1992 <oldiprillo7@gmail.com>
2021-06-05 12:20:45 +0530

    Update: app-env with new variable

--- src/AppModule/config/app-env.ts
+++ src/AppModule/config/app-env.ts
@@ -1,6 +1,7 @@
 export const API_HOST = process.env.REACT_APP_API_HOST || "/api";
 export const AUTH_TOKEN_KEY = process.env.REACT_APP_AUTH_TOKEN_KEY || "es_token";
 export const AUTH_USER_KEY = process.env.REACT_APP_AUTH_USER_KEY|| "es_user";
+export const AUTH_CHOSEN_CONTAINER = process.env.REACT_APP_AUTH_CHOSEN_CONTAINER|| "es_chosen_container";
 /**
  * @description This flag use to detect, what kind of response type is acceptable,
  * so based on that response transformers works
