c4efa24a01c503ae9da1939ee282bd1d479c7c2f
oldi1992 <oldiprillo7@gmail.com>
2021-06-05 17:51:08 +0530

    Fix: structure

--- src/AdminModule/pages/TranslationPage/AppTranslationToolbar.tsx
+++ src/AdminModule/pages/TranslationPage/AppTranslationToolbar.tsx
@@ -43,63 +43,61 @@ export const AppTranslationToolbar: FC<AppTranslationToolbarProps> = ({
     });
 
     return (
-        <Col className={"  translation-page mt-2 "}>
-            <Row>
-                <Col md={3} sm={6} className="px-1">
-                    <AppTagSelectDropDown
-                        options={values}
-                        selectedItems={selectedValues}
-                        label={label}
-                        onChange={onChangeSelect}
-                    />
-                </Col>
-                <Col md={3} sm={6} className="px-1">
-                    <AppFormDropdown
-                        id="where-filter"
-                        defaultValue={{
-                            label: "Translation Key",
+        <Row className={"translation-page d-flex justify-content-end mt-2"}>
+            <Col md={3} sm={6} className="px-1">
+                <AppTagSelectDropDown
+                    options={values}
+                    selectedItems={selectedValues}
+                    label={label}
+                    onChange={onChangeSelect}
+                />
+            </Col>
+            <Col md={3} sm={6} className="px-1">
+                <AppFormDropdown
+                    id="where-filter"
+                    defaultValue={{
+                        label: "Translation Key",
+                        value: "2",
+                    }}
+                    options={[
+                        {
+                            label: "Translation Group",
                             value: "2",
-                        }}
-                        options={[
-                            {
-                                label: "Translation Group",
-                                value: "2",
-                            },
-                            {
-                                label: "Translation Key",
-                                value: "3",
-                            },
-                            {
-                                label: "Translation Value",
-                                value: "4",
-                            },
-                        ]}
-                    />
-                </Col>
-                <Col md={3} sm={6} className="px-1 mt-sm-2 mt-md-0">
-                    <InputGroup>
-                        <InputGroup.Prepend>
-                            <InputGroup.Text id="basic-addon1">
-                                <AppIcon name="Search" />
-                            </InputGroup.Text>
-                        </InputGroup.Prepend>
-                        <Form.Control
-                            placeholder="Search ..."
-                            type={"search"}
-                        ></Form.Control>
-                    </InputGroup>
-                </Col>
-                <Col md={3} sm={6} className="px-1 mt-sm-2  mt-md-0">
-                    <AppButton
-                        variant={"secondary"}
-                        className="justify-content-center p-1"
-                        block
-                        onClick={onCreateNew}
-                    >
-                        + New Group Key
-                    </AppButton>
-                </Col>
-            </Row>
-        </Col>
+                        },
+                        {
+                            label: "Translation Key",
+                            value: "3",
+                        },
+                        {
+                            label: "Translation Value",
+                            value: "4",
+                        },
+                    ]}
+                />
+            </Col>
+            <Col md={3} sm={6} className="px-1 mt-sm-2 mt-md-0">
+                <InputGroup>
+                    <InputGroup.Prepend>
+                        <InputGroup.Text id="basic-addon1">
+                            <AppIcon name="Search" />
+                        </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <Form.Control
+                        placeholder="Search ..."
+                        type={"search"}
+                    ></Form.Control>
+                </InputGroup>
+            </Col>
+            <Col md="auto" sm={6} className=" my-2 my-md-0">
+                <AppButton
+                    variant={"secondary"}
+                    className="justify-content-center"
+                    block
+                    onClick={onCreateNew}
+                >
+                    + Create
+                </AppButton>
+            </Col>
+        </Row>
     );
 };
