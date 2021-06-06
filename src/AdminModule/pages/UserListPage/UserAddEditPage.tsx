8a20a42b237eceef2dfa7cbc402357ec262d478a
oldi1992 <oldiprillo7@gmail.com>
2021-06-07 03:29:00 +0530

    Fix: - hide messages - create value

--- src/AdminModule/pages/UserListPage/UserAddEditPage.tsx
+++ src/AdminModule/pages/UserListPage/UserAddEditPage.tsx
@@ -42,7 +42,7 @@ import {
 import { schema } from "./schema";
 import { CONSTANTS } from "../../../config";
 
-const { TIMEZONE } = CONSTANTS.User;
+const { TIMEZONE, SOURCE } = CONSTANTS.User;
 
 const options = TIMEZONE.map((value: string) => ({
     value,
@@ -166,7 +166,11 @@ export const UserAddEditPage: FC<RouteComponentProps> = ({
         });
         formData.userGroups = userGroupsSelectedItems;
         formData.image_name = "";
-        formData.source = "SOURCE_CREATE";
+
+        if (!isEditMode) {
+            formData.source = SOURCE.SOURCE_CREATE;
+        }
+
         formData.status = "active";
 
         formData.userFieldValues = getDynamicFileds(formData.userField);
