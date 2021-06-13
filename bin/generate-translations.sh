049da0c122fa31fd75a27a847a60adecc1ef7061
oldi1992 <oldiprillo7@gmail.com>
2021-06-13 10:23:23 +0530

    Add: autocopy translation files to build dir

--- bin/generate-translations.sh
+++ bin/generate-translations.sh
@@ -2,10 +2,11 @@
 source ./bin/dotenv
 .env --file .env.local get REACT_APP_API_HOST
 ES_API_HOST=$REPLY
-ES_CONTAINERS_ID=(1 2)
-ES_LOCALES=(en de)
+ES_CONTAINERS_ID=(1 2 3)
+ES_LOCALES=(en de fr)
 GENERATE_TRANSLATIONS="${ES_API_HOST}/generate/translations"
 TRANSLATION_DUMP_PATH="./src/translations"
+TRANSLATION_BUILD_DUMP_PATH="./build/static/translations"
 
 for containerId in "${ES_CONTAINERS_ID[@]}"
 do
@@ -13,5 +14,7 @@ do
   do
     mkdir -p "${TRANSLATION_DUMP_PATH}/${containerId}"
     wget "${GENERATE_TRANSLATIONS}/${containerId}/${locale}" -O "${TRANSLATION_DUMP_PATH}/${containerId}/${locale}.json"
+    mkdir -p ${TRANSLATION_BUILD_DUMP_PATH}/${containerId}
+    cp "${TRANSLATION_DUMP_PATH}/${containerId}/${locale}.json" "${TRANSLATION_BUILD_DUMP_PATH}/${containerId}/"
   done
 done
