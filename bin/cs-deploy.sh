9ff0429fac4797cd4ca8db507214c7cb8397b03c
oldi1992 <oldiprillo7@gmail.com>
2021-06-14 19:11:28 +0530

    Fix: yarn cmd name

--- bin/cs-deploy.sh
+++ bin/cs-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git pull
-yarnpkg install --https_proxy http://10.8.3.81:8080 --http_proxy http://10.8.3.81:8080
+yarn install --https_proxy http://10.8.3.81:8080 --http_proxy http://10.8.3.81:8080
 bash ./bin/generate-jsons.sh
 bash ./bin/generate-translations.sh
-yarnpkg build
+yarn build
