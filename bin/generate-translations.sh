98f64e873de7f2f59a0daade7b44ef166aef410e
oldi1992 <oldiprillo7@gmail.com>
2021-06-18 19:04:07 +0530

    Update: generate-translations.sh

--- bin/generate-translations.sh
+++ bin/generate-translations.sh
@@ -7,10 +7,15 @@ ES_LOCALES=(en de fr it)
 GENERATE_TRANSLATIONS="${ES_API_HOST}/generate/translations"
 TRANSLATION_DUMP_PATH="./src/translations"
 TRANSLATION_BUILD_DUMP_PATH="./build/static/translations"
-TRANSLATION_DEFAULT_EXPORT_FILE="${TRANSLATION_DUMP_PATH}/index.ts";
+TRANSLATION_DEFAULT_EXPORT_FILE="${TRANSLATION_DUMP_PATH}/index.ts"
+GENERATE_CONTAINERS="${ES_API_HOST}/generate/containers";
 
 touch "${TRANSLATION_DEFAULT_EXPORT_FILE}"
 echo -n "" > "${TRANSLATION_DEFAULT_EXPORT_FILE}"
+
+mapfile -t ES_CONTAINERS_ID < <(
+    wget -qO- "${GENERATE_CONTAINERS}" | jq -r '.[]|.'
+)
 
 for containerId in "${ES_CONTAINERS_ID[@]}"
 do
@@ -18,6 +23,11 @@ do
   mkdir -p ${TRANSLATION_BUILD_DUMP_PATH}/${containerId}
   touch "${TRANSLATION_DUMP_PATH}/${containerId}/index.ts"
   echo -n "" > "${TRANSLATION_DUMP_PATH}/${containerId}/index.ts"
+
+  mapfile -t ES_LOCALES < <(
+    wget -qO- "${GENERATE_CONTAINERS}/${containerId}" | jq -r '.[]|.'
+  )
+
   for locale in "${ES_LOCALES[@]}"
   do
     wget "${GENERATE_TRANSLATIONS}/${containerId}/${locale}" -O "${TRANSLATION_DUMP_PATH}/${containerId}/${locale}.json"
