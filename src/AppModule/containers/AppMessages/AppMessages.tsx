3ed0d34a56435cfc541ae676b5b175f7f424429a
oldi1992 <oldiprillo7@gmail.com>
2021-07-16 18:25:48 +0530

    Fix: chat enable/disable after detecting feature, role and flags

--- src/AppModule/containers/AppMessages/AppMessages.tsx
+++ src/AppModule/containers/AppMessages/AppMessages.tsx
@@ -1,13 +1,20 @@
 import React, { FC } from "react";
 import { AppChatOneToOne } from "../AppChatOneToOne";
 import { AppMessageInbox } from "../AppMessageInbox";
+import { useCheckFeature } from "../../hooks/useCheckFeature";
 
 export interface AppMessagesProps {
     disable?: boolean;
 }
 
 export const AppMessages: FC<AppMessagesProps> = ({ disable }) => {
+    const { isChatEnable } = useCheckFeature();
+
     if (disable) {
+        return null;
+    }
+
+    if (!isChatEnable()) {
         return null;
     }
 
---
+++ src/AppModule/hooks/useCheckFeature.ts
@@ -0,0 +1,35 @@
+import { useGlobalData } from "../contexts";
+import { useAuthState } from "./useAuthState";
+import { Configuration } from "../../AdminModule/models";
+import { CONSTANTS } from "../../config";
+
+const {
+    Role: { ROLE },
+} = CONSTANTS;
+
+export function useCheckFeature() {
+    const { container } = useGlobalData();
+    const { user, role } = useAuthState();
+    const configuration: Configuration = (container?.configuration as unknown) as Configuration;
+
+    const isChatEnable = (): boolean => {
+        if (configuration?.isChatEnable) {
+            if (role === ROLE.ROLE_READER) {
+                return false;
+            }
+            if (user.isDisplayAsGuest) {
+                return false;
+            }
+            if (!user.isAllowCommunication) {
+                return true;
+            }
+            return true;
+        }
+
+        return false;
+    };
+
+    return {
+        isChatEnable,
+    };
+}
