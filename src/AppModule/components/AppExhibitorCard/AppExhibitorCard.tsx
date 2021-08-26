ed7189baf7e4eccd0f240221f0e022223aee2c69
oldi1992 <oldiprillo7@gmail.com>
2021-08-26 16:38:53 +0530

    Fix: Issue#SOW5-355

--- src/AppModule/components/AppExhibitorCard/AppExhibitorCard.tsx
+++ src/AppModule/components/AppExhibitorCard/AppExhibitorCard.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { Link } from "@reach/router";
 import { Col } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
@@ -8,9 +8,10 @@ import {
     ExhibitorLogoPosterFileInfo,
 } from "../../../config";
 import { useBuildAssetPath } from "../../hooks";
-import { AppSessionUsers } from "../AppSessionUsers";
 import "./assets/scss/style.scss";
 import placeholder from "../../assets/images/imgthumb.svg";
+import { AppUserListItem } from "../AppUserListItem";
+import { AppShowUserListPopup } from "../AppShowUserListPopup";
 
 export interface AppExhibitorCardProps {
     data: PExhibitor;
@@ -26,6 +27,9 @@ export const AppExhibitorCard: FC<AppExhibitorCardProps> = ({
 }): JSX.Element => {
     const { t } = useTranslation();
     const { id, name, coverImageName, isVisible, logoImageName } = data;
+    const [showMore, isShowMore] = useState<boolean>(false);
+    const users = data?.members ?? [];
+    const limited = users.slice(0, 3);
 
     const imagePath = useBuildAssetPath(
         ExhibitorPosterFileInfo,
@@ -56,10 +60,12 @@ export const AppExhibitorCard: FC<AppExhibitorCardProps> = ({
                         <div className="inner-container--banner--button">
                             {!isVisible ? (
                                 <span className="live-now-btn mr-3">
+                                    <i className={"fak fa-times-light"}></i>
                                     {t("exhibitor.list:badge.inActive")}
                                 </span>
                             ) : (
                                 <span className="archived-btn mr-3">
+                                    <i className="fak fa-check-circle-regular"></i>
                                     {t("exhibitor.list:badge.active")}
                                 </span>
                             )}
@@ -91,15 +97,64 @@ export const AppExhibitorCard: FC<AppExhibitorCardProps> = ({
                             <h2>{name}</h2>
                         </Link>
                     </Col>
-                    <AppSessionUsers
-                        xl={12}
-                        lg={12}
-                        md={12}
-                        sm={12}
-                        selectedUsers={data.members as User[]}
-                        title={t("exhibitor.list:label.members")}
-                        icon="speakers"
-                    />
+                    <Col className={"p-0"}>
+                        <AppShowUserListPopup
+                            show={showMore}
+                            handleClose={isShowMore}
+                            users={users as User[]}
+                            icon={"atendees-cs"}
+                            title={"exhibitor.listCard:section.title.members"}
+                        />
+                        <div className="inner-container--det--content mt-0">
+                            {limited?.length > 0 && (
+                                <>
+                                    <div className="inner-container--det--content--title">
+                                        <i className="fak fa-atendees-cs"></i>
+                                        <h6 className="mb-0 pl-0">
+                                            {t(
+                                                "exhibitor.list:section.title.members"
+                                            )}
+                                        </h6>
+                                    </div>
+                                    <div
+                                        style={{
+                                            overflow: "hidden",
+                                        }}
+                                        className="inner-container--det--content--speakers mt-2 ml-2"
+                                    >
+                                        {limited?.map((e: any, i: number) => {
+                                            return (
+                                                <AppUserListItem
+                                                    key={i}
+                                                    user={e as User}
+                                                />
+                                            );
+                                        })}
+                                    </div>
+                                    <div className="inner-container--det--content--more  ml-4 mt-1">
+                                        {users.length > 3 && (
+                                            <a
+                                                href={"#"}
+                                                onClick={(e) => {
+                                                    e.stopPropagation();
+                                                    e.preventDefault();
+                                                    isShowMore(!showMore);
+                                                }}
+                                            >
+                                                {showMore
+                                                    ? `- ${t(
+                                                          "exhibitor.list:section.showLess"
+                                                      )}`
+                                                    : `+ ${t(
+                                                          "exhibitor.list:section.showMore"
+                                                      )}`}
+                                            </a>
+                                        )}
+                                    </div>
+                                </>
+                            )}
+                        </div>
+                    </Col>
                 </div>
             </Col>
         </Col>
