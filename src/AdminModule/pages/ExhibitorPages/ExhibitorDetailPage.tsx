b8b9810c0f0e16e4dfc9230cbcbc495230c9f772
oldi1992 <oldiprillo7@gmail.com>
2021-08-27 10:59:15 +0530

    Add: QA container to Exhibitor details on admin side

--- src/AdminModule/pages/ExhibitorPages/ExhibitorDetailPage.tsx
+++ src/AdminModule/pages/ExhibitorPages/ExhibitorDetailPage.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useEffect, useState, Fragment } from "react";
 import { Link, RouteComponentProps, useParams } from "@reach/router";
 import { useTranslation } from "react-i18next";
 import "./assets/scss/detail.scss";
@@ -8,8 +8,9 @@ import {
     AppCard,
     AppSessionUsers,
     AppButton,
+    AppQuestionsAndAnswers,
 } from "../../../AppModule/components";
-import { errorToast } from "../../../AppModule/utils";
+import { errorToast, getBGStyle } from "../../../AppModule/utils";
 import { ExhibitorApi } from "../../apis";
 import { Exhibitor, User } from "../../models";
 import {
@@ -17,13 +18,15 @@ import {
     ExhibitorLogoPosterFileInfo,
 } from "../../../config";
 import placeholder from "../../../AppModule/assets/images/imgthumb.svg";
-import { useBuildAssetPath } from "../../../AppModule/hooks";
+import { useAuthState, useBuildAssetPath } from "../../../AppModule/hooks";
+import { ExhibitorCommentsAPI } from "../../../AppModule/apis";
 
 export const ExhibitorDetailPage: FC<RouteComponentProps> = (): JSX.Element => {
     const { t } = useTranslation();
     const { id } = useParams();
     const [loading, isLoading] = useState<boolean>(true);
     const [data, setData] = useState<Exhibitor>();
+    const { containerId } = useAuthState();
     const imagePath = useBuildAssetPath(ExhibitorPosterFileInfo);
     const logoPath = useBuildAssetPath(ExhibitorLogoPosterFileInfo);
 
@@ -45,20 +48,23 @@ export const ExhibitorDetailPage: FC<RouteComponentProps> = (): JSX.Element => {
         return <AppLoader />;
     }
 
-    const style = data?.logoImageName
-        ? {
-              backgroundImage: `url(${logoPath}/${data?.logoImageName})`,
-          }
-        : {
-              backgroundImage: `url(${placeholder})`,
-          };
+    const style = getBGStyle(logoPath, data?.logoImageName ?? "");
 
     return (
-        <>
-            <div className="row m-0">
-                <div className="col-md-12 col-sm-12 col-xl-8 p-3 p-lg-4">
-                    <div className="row m-0 card mb-3 mb-lg-4">
-                        <div className="exhibitors-header col-12">
+        <Fragment>
+            <Row className="m-0">
+                <Col
+                    className={
+                        data?.isCommentEnable
+                            ? "pl-0 pr-0 pr-lg-3 comment-enable"
+                            : "px-0"
+                    }
+                    md={12}
+                    sm={12}
+                    lg={data?.isCommentEnable ? 8 : 12}
+                >
+                    <Row className="m-0 card mb-3 mb-lg-4">
+                        <Col className="exhibitors-header" xs={12}>
                             <div className="row exhibitors-header--detail mb-3 px-2 pt-4">
                                 <div className="col-12 exhibitors-header--detail--buttons d-flex">
                                     <Link
@@ -87,8 +93,8 @@ export const ExhibitorDetailPage: FC<RouteComponentProps> = (): JSX.Element => {
                                     />
                                 </div>
                             </div>
-                        </div>
-                    </div>
+                        </Col>
+                    </Row>
                     {data?.members && data?.members.length > 0 ? (
                         <AppCard>
                             <Row className="m-0 mb-3 mb-lg-4">
@@ -157,8 +163,23 @@ export const ExhibitorDetailPage: FC<RouteComponentProps> = (): JSX.Element => {
                             </Row>
                         </AppCard>
                     ) : null}
-                </div>
-            </div>
-        </>
+                </Col>
+                {data?.isCommentEnable && (
+                    <Col md={12} sm={12} lg={4} className="pr-0 pl-0 pl-lg-3">
+                        <AppQuestionsAndAnswers
+                            name={t(
+                                "exhibitor.detail:section.questionAndAnswers"
+                            )}
+                            parentId={id}
+                            socketParentId={`exhibitor_${id}`}
+                            container={containerId}
+                            commentsAPI={ExhibitorCommentsAPI}
+                            parentElement="/api/exhibitor_comments"
+                            mainElement="api/exhibitors"
+                        />
+                    </Col>
+                )}
+            </Row>
+        </Fragment>
     );
 };
--- src/AppModule/components/AppQuestionsAndAnswers/AppQuestionsAndAnswers.tsx
+++ src/AppModule/components/AppQuestionsAndAnswers/AppQuestionsAndAnswers.tsx
@@ -3,12 +3,7 @@ import { Col } from "react-bootstrap";
 import { AppСhoseMethodMessage } from "../AppСhoseMethodMessage";
 import { AppQAThread } from "../AppQAThread";
 import { errorToast } from "../../utils";
-
-import {
-    useAuthState,
-    useIsGranted,
-    useSessionSocketEvents,
-} from "../../hooks";
+import { useAuthState, useIsGranted, useQASocketEvents } from "../../hooks";
 import { socket, EVENTS } from "../../socket";
 
 import "./assets/scss/style.scss";
@@ -105,19 +100,19 @@ export const AppQuestionsAndAnswers: FunctionComponent<QuestionAndAnswersProps> 
     const [page, setPage] = useState<number>(1);
     const [, setTotalItems] = useState<number>(1);
     const {
-        emitPostNewSessionQa,
-        emitJoinSessionQa,
-        emitLeaveSessionQa,
-        emitDeleteSessionQa,
-        emitEditSessionQa,
-    } = useSessionSocketEvents();
+        emitPostNewQA,
+        emitJoinQA,
+        emitLeaveQA,
+        emitDeleteQA,
+        emitEditQA,
+    } = useQASocketEvents();
     const { user } = useAuthState();
     const isGranted = useIsGranted("ROLE_USER");
     useEffect(() => {
-        if (socketParentId) emitJoinSessionQa(socketParentId);
+        if (socketParentId) emitJoinQA(socketParentId);
 
         return () => {
-            emitLeaveSessionQa(socketParentId);
+            emitLeaveQA(socketParentId);
         };
     }, [socketParentId]);
 
@@ -204,7 +199,7 @@ export const AppQuestionsAndAnswers: FunctionComponent<QuestionAndAnswersProps> 
                     errorToast(errorMessage);
                 }
                 if (response) {
-                    emitPostNewSessionQa(socketParentId, user, response, null);
+                    emitPostNewQA(socketParentId, user, response, null);
                     getCurrentQestionsAndAnswersThread(1);
                 }
             });
@@ -229,7 +224,7 @@ export const AppQuestionsAndAnswers: FunctionComponent<QuestionAndAnswersProps> 
                     errorToast(errorMessage);
                 }
                 if (response) {
-                    emitPostNewSessionQa(
+                    emitPostNewQA(
                         socketParentId,
                         user,
                         {
@@ -262,7 +257,7 @@ export const AppQuestionsAndAnswers: FunctionComponent<QuestionAndAnswersProps> 
                     errorToast(errorMessage);
                 }
                 if (response) {
-                    emitEditSessionQa(socketParentId, user, response, null);
+                    emitEditQA(socketParentId, user, response, null);
                     getCurrentQestionsAndAnswersThread(1);
                 }
             });
@@ -270,7 +265,7 @@ export const AppQuestionsAndAnswers: FunctionComponent<QuestionAndAnswersProps> 
 
     const deleteQuestion = (qId: number) => {
         commentsAPI.deleteById(qId).then(() => {
-            emitDeleteSessionQa(socketParentId, qId);
+            emitDeleteQA(socketParentId, qId);
             getCurrentQestionsAndAnswersThread(1);
         });
     };
