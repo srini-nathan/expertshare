9637f86621ae600cbd9cc334209fea5d95c9b393
oldi1992 <oldiprillo7@gmail.com>
2021-09-01 19:37:20 +0530

    Fix: roles

--- src/AdminModule/pages/ExhibitorPages/ExhibitorListPage.tsx
+++ src/AdminModule/pages/ExhibitorPages/ExhibitorListPage.tsx
@@ -57,6 +57,11 @@ export const ExhibitorListPage: FC<RouteComponentProps> = (): JSX.Element => {
                 isVisible,
                 ...params,
             };
+        } else if (!isGrantedControl) {
+            params = {
+                isVisible: true,
+                ...params,
+            };
         }
         ExhibitorApi.find<Exhibitor>(
             page,
