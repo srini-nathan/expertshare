0649d1b8265f26edb8b9c95196e9593eb6ff937f
erfanesfahani <erfaanesfahani@gmail.com>
2021-09-03 11:26:20 +0300

    set fixed hight for elements

--- src/AppModule/components/AppExhibitorProductCard/AppExhibitorProductCard.tsx
+++ src/AppModule/components/AppExhibitorProductCard/AppExhibitorProductCard.tsx
@@ -32,6 +32,7 @@ export const AppExhibitorProductCard: FC<AppExhibitorProductCardProps> = ({
             className="exhibitors-products--content--item product mb-4"
             xs={12}
             sm={6}
+            md={6}
             lg={4}
         >
             <div className="inner-content card p-3">
@@ -64,6 +65,14 @@ export const AppExhibitorProductCard: FC<AppExhibitorProductCardProps> = ({
                         <h2 className="mb-0">{name}</h2>
                     </a>
                 </div>
+                <div className="inner-content--desc mb-2">
+                    <p className="mb-0">
+                        Your Platform for Virtual Events Expertshare is a
+                        web-based conference and collaboration platform for
+                        virtual events. It combines solutions for e-learning,
+                        online conferences, virtual events with livestream and
+                    </p>
+                </div>
                 {/* <div className="inner-content--category mb-3">
                     <a href="#">
                         <h3 className="mb-0">Category</h3>
@@ -72,15 +81,18 @@ export const AppExhibitorProductCard: FC<AppExhibitorProductCardProps> = ({
                 <div className="inner-content--price">
                     <span> {price} </span>
                 </div>
-                {isCta ? (
-                    <a
-                        className="btn btn-secondary"
-                        href={ctaUrl}
-                        target="_blank"
-                    >
-                        {ctaLabel}
-                    </a>
-                ) : null}
+                <div className="inner-content--buttons">
+                    {isCta ? (
+                        <a
+                            className="btn btn-secondary mb-2"
+                            href={ctaUrl}
+                            target="_blank"
+                        >
+                            {ctaLabel}
+                        </a>
+                    ) : null}
+                    <a className="btn btn-secondary">Document</a>
+                </div>
             </div>
         </Col>
     );
