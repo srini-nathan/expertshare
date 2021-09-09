155fec28c0e7d4549074205f0aa1360df97dd9dc
Parth Modi <parthmodi.developer@gmail.com>
2021-09-09 15:30:37 +0530

    feat: made visualiser dynamic

--- src/AdminModule/components/waveForm/Waveform.tsx
+++ src/AdminModule/components/waveForm/Waveform.tsx
@@ -1,6 +1,4 @@
-/* eslint-disable */
-
-import React, { FC, useState, useEffect, useRef } from "react";
+import React, { FC, useState, useRef } from "react";
 import AudioSpectrum from "react-audio-spectrum";
 import "./waveform.scss";
 
@@ -12,12 +10,18 @@ interface WaveformProps {
 const Waveform: FC<WaveformProps> = ({ url, loop }) => {
     const audioRef: { current: any } = useRef(null);
     const [playing, setPlaying] = useState(false);
+    const [captureImage, setCaptureImage] = useState(null);
     const [mute, setMute] = useState(false);
 
     // const url = "https://www.mfiles.co.uk/mp3-downloads/gs-cd-track2.mp3";
 
     const onPlayPause = () => {
-        if(audioRef.current) {
+        const el = document.getElementById("audio-canvas") as HTMLCanvasElement;
+        if (el) {
+            const image = el.toDataURL("image/png");
+            setCaptureImage(image);
+        }
+        if (audioRef.current) {
             if (playing) {
                 audioRef.current.pause();
                 setPlaying(false);
@@ -28,12 +32,25 @@ const Waveform: FC<WaveformProps> = ({ url, loop }) => {
         }
     };
 
+    const isMusicPlaying = () => {
+        return playing && !mute;
+    };
+
     const onVolumeChange = () => {
+        const el = document.getElementById("audio-canvas") as HTMLCanvasElement;
+        if (el) {
+            const image = el.toDataURL("image/png");
+            setCaptureImage(image);
+        }
         setMute(!mute);
     };
 
     return (
-        <div className="waveformContainer">
+        <div
+            className={`waveformContainer ${
+                isMusicPlaying() ? "playing" : "not-playing"
+            }`}
+        >
             <div className="playButton" onClick={onPlayPause}>
                 {playing ? (
                     <i className="far fa-pause-circle" />
@@ -50,20 +67,21 @@ const Waveform: FC<WaveformProps> = ({ url, loop }) => {
                 ref={audioRef}
                 loop={loop}
             />
+            {!isMusicPlaying() && <img src={captureImage} alt="" />}
             <AudioSpectrum
                 id="audio-canvas"
                 height={45}
-                width={220}
+                width={210}
                 audioId="audio-element"
-                capHeight={0}
+                capHeight={2}
                 meterWidth={2}
                 meterCount={512}
-                meterColor={[
-                    { stop: 0.5, color: "#0CD7FD" },
-                ]}
+                silent={true}
+                meterColor={[{ stop: 0.5, color: "#0CD7FD" }]}
                 gap={4}
             />
-            <div className="playButton" onClick={onVolumeChange}>
+
+            <div className="playButton margin-left" onClick={onVolumeChange}>
                 {mute ? (
                     <i className="fas fa-volume-mute" />
                 ) : (
