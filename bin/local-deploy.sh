c9c121d0b39d2ba11da2633cebb1bf7afd9c65eb
oldi1992 <oldiprillo7@gmail.com>
2021-09-24 17:54:56 +0530

    Fix: remove css fix from local deploy

--- bin/local-deploy.sh
+++ bin/local-deploy.sh
@@ -5,6 +5,5 @@ yarn run fix:config
 bash "./bin/generate-translations.sh"
 yarn run fix:translations
 bash "./bin/generate-styles.sh"
-yarn run fix:css
 cp -r ./src/translations/ ./build/static
 cp ./src/config/css/* ./build/static/css
