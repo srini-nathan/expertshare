cd61fc271ee3e056d15242c8a9da7c45ed578edc
oldi1992 <oldiprillo7@gmail.com>
2021-09-29 15:24:22 +0530

    WIP

--- src/AppModule/apis/MeetingBookingApi.ts
+++ src/AppModule/apis/MeetingBookingApi.ts
@@ -1,5 +1,8 @@
+import { AxiosError, Canceler } from "axios";
 import { EntityAPI } from "./EntityAPI";
 import { ROUTES } from "../../config";
+import { FinalResponse, ListResponse, ServerError } from "../models";
+import { onFindAllResponseHydra, onFindAllResponseJson } from "./transformer";
 
 const {
     api_meeting_bookings_delete_item: API_DELETE_ITEM,
@@ -8,6 +11,7 @@ const {
     api_meeting_bookings_put_item: API_PUT_ITEM,
     api_meeting_bookings_patch_item: API_PATCH_ITEM,
     api_meeting_bookings_post_collection: API_POST_COLLECTION,
+    api_meeting_bookings_get_my_collection: API_GET_MY_COLLECTION,
 } = ROUTES;
 
 export abstract class MeetingBookingApi extends EntityAPI {
@@ -22,4 +26,39 @@ export abstract class MeetingBookingApi extends EntityAPI {
     protected static PATCH_ITEM = API_PATCH_ITEM;
 
     protected static DELETE_ITEM = API_DELETE_ITEM;
+
+    public static async getMyBookings<E>(
+        page = 1,
+        extraParams = {},
+        cancelToken?: (c: Canceler) => void
+    ): Promise<FinalResponse<ListResponse<E> | null>> {
+        const source = this.createCancelTokenSource();
+
+        if (cancelToken) {
+            cancelToken(source.cancel);
+        }
+
+        return this.makeGet<E>(
+            API_GET_MY_COLLECTION,
+            {
+                ...extraParams,
+                page,
+            },
+            {
+                cancelToken: source.token,
+            }
+        )
+            .then(({ data }) => {
+                const list = this.acceptHydra
+                    ? onFindAllResponseHydra<E>(data)
+                    : onFindAllResponseJson<E>(data);
+                return Promise.resolve(
+                    new FinalResponse<ListResponse<E>>(list)
+                );
+            })
+            .catch((error: AxiosError | ServerError) => {
+                const { message } = error;
+                return Promise.resolve(new FinalResponse(null, message));
+            });
+    }
 }
